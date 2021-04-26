6cfdbfd4eb471991af4a80f6cfa8431b34341d30
Sienna94 <csj19940407@gmail.com>
2021-04-26 23:58:33 +0900

    테스트테스트

--- app/src/main/java/com/example/kotlinpractice/MyGLRenderer.kt
+++ app/src/main/java/com/example/kotlinpractice/MyGLRenderer.kt
@@ -8,7 +8,8 @@ import javax.microedition.khronos.opengles.GL10
 class MyGLRenderer : GLSurfaceView.Renderer {
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
         //프레임의 배경색상 설정하기
-        GLES20.glClearColor(0.0f, 1.0f, 0.0f, 0.0f);
+        //커밋용 커밋커밋커밋
+        GLES20.glClearColor(0.0f, 1.3f, 1.2f, 0.0f);
     }
 
     override fun onDrawFrame(gl: GL10?) {
